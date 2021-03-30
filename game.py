785d0d36da0d43cc6bd75e84b0f1ac2c20a2c19f
LironBenTora <77121549+LironBenTora@users.noreply.github.com>
2021-03-30 20:57:06 +0300

    Update game.py

--- game.py
+++ game.py
@@ -9,7 +9,10 @@ surface = pygame.display.set_mode((600, 600))    # The surface of the game
 pygame.display.set_caption('Tic-tac-toe')   # The name of the surface
 
 grid = Grid()
+
+
 running = True
+player = "X"
 
 while running:
     for event in pygame.event.get():
@@ -18,8 +21,13 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                print(pos[0] // 200, pos[1] // 200)
+                grid.get_mouse(pos[0] // 200, pos[1] // 200, player)
+                if player == "X":
+                    player = "O"
+                else:
+                    player = "X"
 
+                grid.print_grid()
 
     surface.fill((0, 0, 0))
 
